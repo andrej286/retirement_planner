848f29a6d9d668e8e11cc58ef8768a7ec829d9d5
andrej286 <mikistojanovski6@gmial.com>
2026-06-04 19:31:44 +0200

    Database initialization

--- RetirementPlannerWeb/src/main/java/com/retirementplanner/web/RetirementPlannerWebApplication.java
+++ RetirementPlannerWeb/src/main/java/com/retirementplanner/web/RetirementPlannerWebApplication.java
@@ -2,8 +2,12 @@ package com.retirementplanner.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.retirementplanner")
+@EnableJpaRepositories(basePackages = "com.retirementplanner.persistence.repository")
+@EntityScan(basePackages = "com.retirementplanner.persistence.entity")
 public class RetirementPlannerWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(RetirementPlannerWebApplication.class, args);
